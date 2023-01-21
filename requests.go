5a36f8e7a055959cc8803cd3d803e73e1d15659e
Kristof Nagy <kristofn@protonmail.ch>
2023-01-22 00:43:01 +0100

    refactor: add baserequest type

---
+++ requests.go
@@ -0,0 +1,14 @@
+package geofox
+
+type Filter string
+
+const (
+	NoFilter  Filter = "NO_FILTER"
+	HVVListed Filter = "HVV_LISTED"
+)
+
+type BaseRequest struct {
+	Language   string `json:"language,omitempty"`
+	Version    int32  `json:"version,omitempty"`
+	FilterType Filter `json:"filterType,omitempty"`
+}
