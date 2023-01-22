a3935488cf3d321ee968889490b7187cfaaca0f3
Kristof Nagy <kristofn@protonmail.ch>
2023-01-22 13:36:00 +0100

    refactor: move handler functions to geofox.go

--- geofox.go
+++ geofox.go
@@ -6,12 +6,16 @@ import (
 	"crypto/hmac"
 	"crypto/sha1"
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"strings"
+
+	"github.com/kristoffn/geofox-go/coordinates"
+	"github.com/kristoffn/geofox-go/model"
 )
 
 const (
@@ -113,4 +117,52 @@ func (a *API) makeRequest(ctx context.Context, method, url string, payload *stri
 		return nil, err
 	}
 	return body, nil
+}
+
+func (a *API) Init(ctx context.Context) (*model.InitResponse, error) {
+	req := model.InitRequest{}
+	reqBytes, err := json.Marshal(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal InitRequest object: %s", err.Error())
+	}
+	payload := strings.NewReader(string(reqBytes))
+
+	responseBytes, err := a.makeRequest(ctx, http.MethodPost, endpointInit, payload)
+	if err != nil {
+		return nil, err
+	}
+
+	var resp model.InitResponse
+	if err = json.Unmarshal(responseBytes, &resp); err != nil {
+		return nil, fmt.Errorf("failed to marshal body bytes: %s", err.Error())
+	}
+	a.initialized = true
+	return &resp, nil
+}
+
+func (a *API) ListStations(ctx context.Context, modTypes []model.ModificationType, coordType coordinates.CoordinateType) (*model.LSResponse, error) {
+	req := model.LSRequest{
+		ModificationTypes: modTypes,
+		CoordinateType:    coordType,
+		FilterEquivalent:  true,
+	}
+	req.Language = model.LanguageGerman
+	req.Version = 51
+
+	reqBytes, err := json.Marshal(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal LSRequest object: %s", err.Error())
+	}
+	payload := strings.NewReader(string(reqBytes))
+
+	responseBytes, err := a.makeRequest(ctx, http.MethodPost, endpointListStations, payload)
+	if err != nil {
+		return nil, err
+	}
+
+	var resp model.LSResponse
+	if err = json.Unmarshal(responseBytes, &resp); err != nil {
+		return nil, fmt.Errorf("failed to marshal body bytes: %s", err.Error())
+	}
+	return &resp, nil
 }
