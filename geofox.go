91f5c7acbfda41548cd47c8a7a77716d20e31448
Kristof Nagy <kristofn@protonmail.ch>
2023-01-22 21:15:20 +0100

    test: init endpoint unit test

--- geofox.go
+++ geofox.go
@@ -138,27 +138,6 @@ func (a *API) makeRequest(ctx context.Context, method, url string, payload *stri
 	return body, nil
 }
 
-func (a *API) Init(ctx context.Context) (*model.InitResponse, error) {
-	req := model.InitRequest{}
-	reqBytes, err := json.Marshal(req)
-	if err != nil {
-		return nil, fmt.Errorf("failed to marshal InitRequest object: %s", err.Error())
-	}
-	payload := strings.NewReader(string(reqBytes))
-
-	responseBytes, err := a.makeRequest(ctx, http.MethodPost, a.BaseURL+"/init", payload)
-	if err != nil {
-		return nil, err
-	}
-
-	var resp model.InitResponse
-	if err = json.Unmarshal(responseBytes, &resp); err != nil {
-		return nil, fmt.Errorf("failed to marshal body bytes: %s", err.Error())
-	}
-	a.initialized = true
-	return &resp, nil
-}
-
 func (a *API) ListStations(ctx context.Context, modTypes []model.ModificationType, coordType coordinates.CoordinateType) (*model.LSResponse, error) {
 	req := model.LSRequest{
 		ModificationTypes: modTypes,
---
+++ handlers.go
@@ -0,0 +1,32 @@
+package geofox
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/kristoffn/geofox-go/model"
+)
+
+func (a *API) Init(ctx context.Context) (*model.InitResponse, error) {
+	req := model.InitRequest{}
+	reqBytes, err := json.Marshal(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal InitRequest object: %s", err.Error())
+	}
+	payload := strings.NewReader(string(reqBytes))
+
+	responseBytes, err := a.makeRequest(ctx, http.MethodPost, a.BaseURL+"/init", payload)
+	if err != nil {
+		return nil, err
+	}
+
+	var resp model.InitResponse
+	if err = json.Unmarshal(responseBytes, &resp); err != nil {
+		return nil, fmt.Errorf("failed to marshal body bytes: %s", err.Error())
+	}
+	a.initialized = true
+	return &resp, nil
+}
