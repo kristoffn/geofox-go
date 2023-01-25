9a5770c43a18280ab39ac6799b2bf440b978a61b
Kristof Nagy <kristofn@protonmail.ch>
2023-01-25 09:48:09 +0100

    refactor: remove models package

--- handlers_test.go
+++ handlers_test.go
@@ -6,8 +6,6 @@ import (
 	"net/http"
 	"testing"
 
-	"github.com/kristoffn/geofox-go/coordinates"
-	"github.com/kristoffn/geofox-go/model"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -15,7 +13,7 @@ func TestAPI_Init(t *testing.T) {
 	// Happy path
 	setupTestServer()
 	testMux.HandleFunc("/init", func(w http.ResponseWriter, r *http.Request) {
-		response := model.InitResponse{
+		response := InitResponse{
 			ReturnCode:     "test-return-code",
 			BeginOfService: "2000-01-01",
 			EndOfService:   "2000-01-02",
@@ -56,9 +54,9 @@ func TestAPI_ListStations(t *testing.T) {
 	// Happy path
 	setupTestServer()
 	testMux.HandleFunc("/listStations", func(w http.ResponseWriter, r *http.Request) {
-		response := model.LSResponse{
+		response := LSResponse{
 			DataReleaseID: "test-data-release-id",
-			Stations: []model.StationListEntry{
+			Stations: []StationListEntry{
 				{}, {}, {},
 			},
 		}
@@ -67,8 +65,8 @@ func TestAPI_ListStations(t *testing.T) {
 		w.Write(responseBytes) //nolint
 	})
 	resp, err := testClient.ListStations(context.Background(),
-		[]model.ModificationType{model.ModificationTypeMain},
-		coordinates.EPSG4326)
+		[]ModificationType{ModTypeMain},
+		EPSG4326)
 	assert.Nil(t, err)
 	assert.Equal(t, "test-data-release-id", resp.DataReleaseID)
 	assert.Equal(t, 3, len(resp.Stations))
@@ -80,8 +78,8 @@ func TestAPI_ListStations(t *testing.T) {
 		w.WriteHeader(http.StatusNotFound)
 	})
 	resp, err = testClient.ListStations(context.Background(),
-		[]model.ModificationType{model.ModificationTypeMain},
-		coordinates.EPSG4326)
+		[]ModificationType{ModTypeMain},
+		EPSG4326)
 	assert.Nil(t, resp)
 	assert.Contains(t, err.Error(), "status code: 404")
 	teardownTestServer()
@@ -92,8 +90,8 @@ func TestAPI_ListStations(t *testing.T) {
 		w.Write([]byte{'r', 'a', 'n', 'd', 'o', 'm'}) //nolint
 	})
 	resp, err = testClient.ListStations(context.Background(),
-		[]model.ModificationType{model.ModificationTypeMain},
-		coordinates.EPSG4326)
+		[]ModificationType{ModTypeMain},
+		EPSG4326)
 	assert.Nil(t, resp)
 	assert.Contains(t, err.Error(), "failed to marshal body bytes")
 	teardownTestServer()
